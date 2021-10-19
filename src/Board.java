f2a9b955ec8f70fa9bc6a418f6cb0c0f3d6f8dcc
harsimrankanwar <harsimran.kanwar@yahoo.ca>
2021-10-19 16:21:41 -0400

    finished enums

--- src/Board.java
+++ src/Board.java
@@ -1,11 +1,30 @@
 public class Board {
     public enum properties {
         MEDITERRANEAN_AVENUE("MEDITERRANEAN AVENUE",60,"brown"),
+        BALTIC_AVENUE("BALTIC AVENUE",60,"brown"),
+        ORIENTAL_AVENUE("ORIENTAL AVENUE",100,"light blue"),
+        VERMONT_AVENUE("VERMONT AVENUE",100,"light blue"),
+        CONNECTICUT_AVENUE("CONNECTICUT AVENUE",120,"light blue"),
+        ST_CHARLES_PLACE("ST. CHARLES PLACE",140,"magenta"),
+        STATES_AVENUE("STATES AVENUE",140,"magenta"),
+        VIRGINIA_AVENUE("VIRGINIA AVENUE",160,"magenta"),
+        ST_JAMES_PLACE("ST. JAMES PLACE",180,"orange"),
+        TENNESSEE("TENNESSEE",180,"orange"),
+        NEW_YORK_AVENUE("NEW YORK AVENUE",200,"orange"),
+        KENTUCKY_AVENUE("KENTUCKY AVENUE",220,"red"),
+        INDIANA_AVENUE("INDIANA AVENUE",220,"red"),
+        ILLINOIS_AVENUE("ILLINOIS AVENUE",240,"red"),
+        ATLANTIC_AVENUE("ATLANTIC AVENUE",260,"yellow"),
+        VENTNOR_AVENUE("VENTNOR AVENUE",260,"yellow"),
+        MARVIN_GARDENS("MARVIN GARDENS",280,"yellow"),
+        PACIFIC_AVENUE("PACIFIC AVENUE",300,"green"),
+        NORTH_CAROLINA_AVENUE("NORTH CAROLINA AVENUE",300,"green"),
+        PENNSYLVANIA_AVENUE("PENNSYLVANIA AVENUE",320,"green"),
+        PARK_PLACE("PARK PLACE",350,"blue"),
+        BOARDWALK("BOARDWALK",400,"blue");
 
-        ;
-
-        properties(String name, int price , String colour) {
-            Property(name,price,colour);
+        properties(String name, int price , String color) {
+            new Property(name,price,color);
         }
     }
 
