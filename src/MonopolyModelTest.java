312c44c41152d4dcdf85b63fe6cd8f7dad34c55d
harsimrankanwar <harsimran.kanwar@yahoo.ca>
2021-11-08 12:09:22 -0500

    created test class for model  - harsi

---
+++ src/MonopolyModelTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MonopolyModelTest {
+
+    @Test
+    public void getPlayers() {
+    }
+
+    @Test
+    public void getPlayer() {
+    }
+
+    @Test
+    public void getDice() {
+    }
+
+    @Test
+    public void getOutputText() {
+    }
+
+    @Test
+    public void getPlayerStatus() {
+    }
+
+    @Test
+    public void getPieces() {
+    }
+}
