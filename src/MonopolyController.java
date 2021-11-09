a5ec932cbfef385b4bbc4275f2722c2b6e701e9d
Evan Smedley <evan.smedley22@gmail.com>
2021-11-08 19:21:35 -0500

    Added error handling for dialog boxes - Evan

--- src/MonopolyController.java
+++ src/MonopolyController.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.util.Objects;
+
 /**
  * WE NEED TO FIX THIS
  * bug where if user presses enter when asked for a username, the username becomes blank, we should prevent this..
@@ -29,22 +31,48 @@ public class MonopolyController {
         for (int i = 0; i < MAX_PLAYERS - 1; i++) {
             options[i] = i + 2;
         }
-        return (int) JOptionPane.showInputDialog(
-                view, "How many players are there?", "Player Selection",
-                JOptionPane.PLAIN_MESSAGE, null, options, 2
-        );
+        boolean validInput = false;
+        Object numPlayersObject = null;
+        String message = "How many players are there?";
+        while (!validInput) {
+            numPlayersObject = JOptionPane.showInputDialog(
+                    view, message, "Player Selection",
+                    JOptionPane.PLAIN_MESSAGE, null, options, 2
+            );
+            if (numPlayersObject == null) {
+                message = "How many players are there?\nPlease press 'Ok', not 'Cancel'";
+            } else {
+                validInput = true;
+            }
+        }
+        return (int) numPlayersObject;
     }
 
     /** Returns the Player object with a username chosen by the user
      * @return the player object
      */
     private Player getPlayerInformation() {
-        String playerName = (String) JOptionPane.showInputDialog(
-                view, "Please choose a name that is 10 characters or less.\n" +
-                "If it is too long it will be truncated.",
-                "Player " + (model.getPlayers().size() + 1) + " Name Selection", JOptionPane.PLAIN_MESSAGE
-        );
-        playerName = playerName.substring(0, Math.min(playerName.length(), 10));
+        boolean validInput = false;
+        String playerName = "";
+        String message = "Please choose a name that is 10 characters or less.\n" +
+                "If it is too long it will be truncated.";
+        while (!validInput) {
+            playerName = (String) JOptionPane.showInputDialog(
+                    view, message,
+                    "Player " + (model.getPlayers().size() + 1) + " Name Selection", JOptionPane.PLAIN_MESSAGE
+            );
+            if (playerName == null) {
+                message = "Please choose a name that is 10 characters or less.\n" +
+                        "If it is too long it will be truncated.\nPlease try again and press 'Ok' not 'Cancel'";
+            } else if (playerName.equals("")) {
+                message = "Please choose a name that is 10 characters or less.\n" +
+                        "If it is too long it will be truncated.\nYour name cannot be blank";
+            } else {
+                validInput = true;
+                playerName = playerName.substring(0, Math.min(playerName.length(), 10));
+            }
+        }
+
         return new Player(playerName);
     }
 }
