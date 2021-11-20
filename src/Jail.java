c5c8fd7c869ec6fc0cd663a23395fd4ff4c9dd33
dorothytran <dorothytraan@gmail.com>
2021-11-20 09:27:14 -0500

    created ai class, added javadocs - doro

--- src/Jail.java
+++ src/Jail.java
@@ -1,15 +1,23 @@
 /**
- * Jail Class that extends the BoardSpace Class
+ * Jail Class that extends the BoardSpace Class for when a player is in a Jail board square
  */
 public class Jail extends BoardSpace{
     private String type;
+
+    /**
+     * Constructor for the Jail Class
+     * @param name String, name of the board square
+     * @param path String, path directory
+     * @param type String, type of property
+     * @param position int, position number on the board
+     */
     public Jail(String name, String path, String type,int position) {
         super(name, type, path, position);
         this.type = type;
     }
 
     /**
-     *
+     * Method returns the  Jail board square type (Just Visiting or Go to Jail)
      * @return
      */
     public String getType(){
@@ -17,8 +25,8 @@ public class Jail extends BoardSpace{
     }
 
     /**
-     *
-     * @return
+     * Overridden displayInfo method that displays info to the player when they are sent to jail
+     * @return String, text info
      */
     @Override
     public String displayInfo() {
---
+++ src/MonopolyAIPlayer.java
@@ -0,0 +1,16 @@
+/**
+ * MonopolyAI Class that handles the AI players of the game
+ */
+public class MonopolyAIPlayer extends Player {
+
+    /**
+     * Constructor for MonopolyAIPlayer class
+     * @param name , name of player
+     */
+    // auto constructor
+    public MonopolyAIPlayer(String name) {
+        super(name);
+    }
+
+
+}
