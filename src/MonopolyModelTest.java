a68233df43b77dad4a2dfa093b3b9b7b54bd1097
dorothytran <dorothytraan@gmail.com>
2021-12-02 19:53:38 -0500

    fixed the previous tests to make them pass, still implementing Go & Jail tests - doro

--- src/MonopolyModelTest.java
+++ src/MonopolyModelTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.*;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MonopolyModelTest {
     MonopolyModel mm; //model attribute
+    BoardSpace[] pieces;
 
     //NOTE: FOR TESTING USE 2 PLAYERS
     @Test
@@ -24,8 +25,9 @@ public class MonopolyModelTest {
         MonopolyFrame monopolyFrame = new MonopolyFrame(mm);
         MonopolyController mc = new MonopolyController(mm,monopolyFrame);
         assertEquals("", mm.getOutputText());
+        String prev = mm.getPlayers().get(mm.getPlayer()).getName();
         mm.play("Pass");
-        assertEquals("Your turn is now over! Passing to next player.\nNow it's " + mm.getPlayers().get(mm.getPlayer()).getName() +  " 's turn!", mm.getOutputText());
+        assertEquals(prev + " has passed their turn! Passing to next player.\n\nNow it's " + mm.getPlayers().get(mm.getPlayer()).getName() +  "'s turn!", mm.getOutputText());
     }
 
     @Test
@@ -33,7 +35,6 @@ public class MonopolyModelTest {
         mm = new MonopolyModel();
         MonopolyFrame monopolyFrame = new MonopolyFrame(mm);
         MonopolyController mc = new MonopolyController(mm,monopolyFrame);
-
         assertEquals(MonopolyModel.Status.UNDECIDED,mm.getPlayerStatus());
         mm.play("Roll Dice");
         assertEquals(MonopolyModel.Status.PLAYING,mm.getPlayerStatus());
@@ -57,11 +58,11 @@ public class MonopolyModelTest {
         MonopolyController mc = new MonopolyController(mm,monopolyFrame);
         mm.play("Roll Dice");
         String prev = mm.getPlayers().get(mm.getPlayer()).getName().toUpperCase(Locale.ROOT);
-        mm.play("Buy Property");
-        assertEquals( prev + "'S TURN:\nSuccessfully purchased!\nNow it's " + mm.getPlayers().get(mm.getPlayer()).getName() +" 's turn!", mm.getOutputText());
-
+        mm.play("Buy");
+        assertEquals( prev + "'S TURN:\nSuccessfully purchased!\n\nNow it's " + mm.getPlayers().get(mm.getPlayer()).getName() +"'s turn!", mm.getOutputText());
     }
 
+    // Not reaching this test again for some reason
     @Test
     public void zBankruptcy() {
         mm =  new MonopolyModel();
@@ -70,8 +71,34 @@ public class MonopolyModelTest {
         Player p1 = mm.getPlayers().get(mm.getPlayer());
         mm.play("Roll Dice");
         p1.doTransaction(1500); //force player to go bankrupt
-        assertEquals(true, mm.checkBankruptcy());
+        assertTrue(mm.checkBankruptcy());
+    }
 
+    @Test
+    public void passedGo() {
+        mm =  new MonopolyModel();
+        MonopolyFrame monopolyFrame = new MonopolyFrame(mm);
+        MonopolyController mc = new MonopolyController(mm,monopolyFrame);
+        Player p1 = mm.getPlayers().get(mm.getPlayer());
+        mm.play("Roll Dice");
+        /*if(pieces[p1.getPosition()] instanceof Property || pieces[p1.getPosition()] instanceof Railroad || pieces[p1.getPosition()] instanceof Utility) {
+            p1.move(25);
+        }*/
+        assertEquals(1700.0, p1.getMoney(), 2);
+    }
+
+    @Test
+    public void jail() {
+        mm =  new MonopolyModel();
+        MonopolyFrame monopolyFrame = new MonopolyFrame(mm);
+        MonopolyController mc = new MonopolyController(mm,monopolyFrame);
+        mm.play("Roll Dice");
+        Player p1 = mm.getPlayers().get(mm.getPlayer());
+        // tryna somehow get them to move to the jail boardsquare but for ours they move up spaces to start the game
+        assertTrue(p1.isInJail());
+    }
+
+    public void moveAI() {
     }
 
 }
